e9cca24044ff68cea905a2ef0a357206161c0625
61ok5 <lok60674211@gmail.com>
2022-01-14 16:54:02 +0800

    k

---
+++ fyp-frontend/src/Home.js
@@ -0,0 +1,23 @@
+import { Container, Grid } from "@mui/material";
+import React from "react";
+
+function Home() {
+    return (
+        <Container>
+            <Grid container>
+                <Grid item>
+                    <img
+                        src="https://media.makeameme.org/created/i-will-buy-5a739f.jpg"
+                    />
+                </Grid>
+                <Grid item>
+                    <img
+                        src="https://media.makeameme.org/created/i-will-buy-5a739f.jpg"
+                    />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Home;
