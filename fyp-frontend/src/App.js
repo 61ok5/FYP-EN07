3e8c6ba8e92ab2d6f3eddd6636052502705205b6
61ok5 <lok60674211@gmail.com>
2022-02-14 16:05:01 +0800

    Setup Done

--- fyp-frontend/src/App.js
+++ fyp-frontend/src/App.js
@@ -1,28 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Header from "./Header";
-import { CssBaseline } from "@mui/material";
-import { height } from "@mui/system";
+import React from 'react';
 
-function App() {
-  return (
-    <div id="App" style={{ minHeight: '100vh', backgroundColor: '#FAFAFA' }}>
-      <Router>
-        <Header />
-        <div style={{ flexGrow: 1 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />}/>
-            <Route path="/resume" element={<Resume />}/> */}
-          </Routes>
-        </div>
-        {/* <Footer /> */}
-      </Router>
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
 export default App;
