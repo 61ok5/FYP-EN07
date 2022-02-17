b4a196969f03dd54f63b72f5af50424199fa8257
61ok5 <lok60674211@gmail.com>
2022-02-17 22:09:30 +0800

    Home Update

--- fyp-frontend/src/container/Header/Header.jsx
+++ fyp-frontend/src/container/Header/Header.jsx
@@ -5,16 +5,16 @@ import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div className="app__header app__wrapper section__padding" id="home">
+  <div className="app__header app__wrapper section__padding app__headerbg" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <SubHeading title="Key To Success" />
+      <h1 className="app__header-h1" style={{ textShadow: '0px 0px 4px rgba(0, 0, 0, 0.5)' }}>Independent Learning</h1>
+      <p className="p__opensans_white" style={{ margin: '2rem 0' }}>Professional mechanical learning algorithms and appropriate course recommendations<br />to unleash your potential. Your future depends on your dreams! </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
+      <img src={images.recom} alt="header_img" />
     </div>
   </div>
 );
