421d96139fcc459c646861fcf620be1b301b75f3
61ok5 <lok60674211@gmail.com>
2022-03-08 14:44:16 +0800

    ahref Update

--- fyp-frontend/src/components/Navbar/Navbar.jsx
+++ fyp-frontend/src/components/Navbar/Navbar.jsx
@@ -10,14 +10,14 @@ const Navbar = () => {
   const location = useLocation();
 
   return (
-    <nav className="app__navbar" style={location.pathname !== '/' && { borderBottom: '0.1rem solid #ebdcac' }}>
+    <nav className="app__navbar" style={location.pathname !== '/' ? { borderBottom: '0.1rem solid #ebdcac' } : {}}>
       <div className="app__navbar-logo">
         <img src={images.gericht} alt="app__logo" />
       </div>
       <ul className="app__navbar-links">
-        <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href="#home">Home</a></li>
-        <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href="#about">About</a></li>
-        <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href="#topics">Topics</a></li>
+        <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href={location.pathname === '/' ? '#home' : '/#home'}>Home</a></li>
+        <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href={location.pathname === '/' ? '#about' : '/#about'}>About</a></li>
+        <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href={location.pathname === '/' ? '#topics' : '/#topics'}>Topics</a></li>
         <li className={location.pathname === '/' ? 'p__opensans_white' : 'p__opensans'}><a href="/hi">Courses</a></li>
         {/* <li className="p__opensans_white"><a href="#contact">Contact</a></li> */}
       </ul>
@@ -33,25 +33,25 @@ const Navbar = () => {
             <MdClose fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
               <li className="p__opensans_white" style={{ textShadow: '0px 0px 3px rgba(0, 0, 0, 0.8)' }}>
-                <a href="#about" onClick={() => setToggleMenu(false)}>
+                <a href={location.pathname === '/' ? '#about' : '/#about'} onClick={() => setToggleMenu(false)}>
                   <img src={images.key} alt="key" style={{ width: '2rem', marginRight: '1rem' }} />
                   About
                 </a>
               </li>
               <li className="p__opensans_white" style={{ textShadow: '0px 0px 3px rgba(0, 0, 0, 0.8)' }}>
-                <a href="#home" onClick={() => setToggleMenu(false)}>
+                <a href={location.pathname === '/' ? '#home' : '/#home'} onClick={() => setToggleMenu(false)}>
                   <img src={images.key} alt="key" style={{ width: '2rem', marginRight: '1rem' }} />
                   Home
                 </a>
               </li>
               <li className="p__opensans_white" style={{ textShadow: '0px 0px 3px rgba(0, 0, 0, 0.8)' }}>
-                <a href="#topics" onClick={() => setToggleMenu(false)}>
+                <a href={location.pathname === '/' ? '#topics' : '/#topics'} onClick={() => setToggleMenu(false)}>
                   <img src={images.key} alt="key" style={{ width: '2rem', marginRight: '1rem' }} />
                   Topics
                 </a>
               </li>
               <li className="p__opensans_white" style={{ textShadow: '0px 0px 3px rgba(0, 0, 0, 0.8)' }}>
-                <a href="#courses" onClick={() => setToggleMenu(false)}>
+                <a href="/hi" onClick={() => setToggleMenu(false)}>
                   <img src={images.key} alt="key" style={{ width: '2rem', marginRight: '1rem' }} />
                   Courses
                 </a>
