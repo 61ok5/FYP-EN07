1ad026f24a28ecfa73ab0485d819c70c481d8248
61ok5 <lok60674211@gmail.com>
2022-04-19 13:45:28 +0800

    Syntax Error Fixed

--- fyp-frontend/src/container/Header/Header.jsx
+++ fyp-frontend/src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Key To Success" />
       <h1 className="app__header-h1" style={{ textShadow: '0px 0px 4px rgba(0, 0, 0, 0.5)' }}>Independent Learning</h1>
-      <p className="p__opensans_white" style={{ margin: '2rem 0' }}>Professional mechanical learning algorithms and appropriate course recommendations<br />to unleash your potential. Your future depends on your dreams! </p>
+      <p className="p__opensans_white" style={{ margin: '2rem 0' }}>Professional machine learning algorithms and appropriate course recommendations<br />to unleash your potential. Your future depends on your dreams! </p>
       <Link to="/course">
         <button type="button" className="custom__button p__opensans_white" style={{ textShadow: '0px 0px 2px rgba(0, 0, 0, 0.6' }}>Check it out</button>
       </Link>
